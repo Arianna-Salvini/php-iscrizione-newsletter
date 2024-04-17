dd383292f723861021888203ce7095e13908b215
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-17 15:52:39 +0200

    Add some layout and style with boothstrap

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-$name = $_GET["name"];
 $email = $_GET["email"];
 
 // Function for email validation
@@ -48,28 +47,33 @@ $message = generateAlertValidation($messageValidation);
 </head>
 
 <body>
-    <header>
-        <nav>
+
+    <header class="py-3 bg-primary fixed-top">
+        <nav class="container text-light">
             Newsletter
         </nav>
     </header>
-    <main>
-        <h2>Iscriviti alla nostra Newsletter</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis blanditiis magnam placeat aliquam. Voluptatibus reiciendis alias non deserunt tempore totam a. Tempora, repudiandae quas autem, dolore consectetur excepturi dolores, expedita quasi a sit quae! Praesentium repellat eligendi necessitatibus cum repudiandae dolor enim ipsum quidem! Earum nemo aliquam voluptas ducimus rem!</p>
-        <!-- form -->
-        <form action="#" method="get">
-            <input type="email" name="email" id="email" placeholder="Inserisci la tua email">
-            <button type="submit">Invia</button>
-        </form>
-        <div class="alert <?php echo $message['alert'] ?> ">
-            <p><?php echo $message['text'] ?></p>
+    <main class="text-center my-5 d-flex justify-content-center align-items-center" style="min-height: calc(100vh - 160px);">
+        <div class="container">
+            <h2 class="text-primary-emphasis ">Iscriviti alla nostra Newsletter</h2>
+            <p class="my-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis blanditiis magnam placeat aliquam. Voluptatibus reiciendis alias non deserunt tempore totam a. Tempora, repudiandae quas autem, dolore consectetur excepturi dolores, expedita quasi a sit quae! Praesentium repellat eligendi necessitatibus cum repudiandae dolor enim ipsum quidem! Earum nemo aliquam voluptas ducimus rem!</p>
+            <!-- form -->
+            <form class="mt-4 pb-3 d-flex justify-content-center" action="#" method="get">
+                <input type="email" name="email" id="email" placeholder="Inserisci la tua email" class="rounded px-4 py-1">
+                <button type="submit" class="btn btn-outline-primary rounded">Invia</button>
+            </form>
+            <div class="alert <?php echo $message['alert'] ?>  d-flex justify-content-center">
+                <p><?php echo $message['text'] ?></p>
+                <button type="button" class="btn-close"></button>
+            </div>
+            <!-- Alert validation -->
         </div>
-        <!-- Alert validation -->
 
     </main>
-    <footer>
-        <span>Boolean © by Classe 119</span>
+    <footer class="py-3 bg-primary fixed-bottom">
+        <div class="container text-light">Boolean © by Classe 119</div>
     </footer>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
